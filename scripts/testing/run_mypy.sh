dbbed1922fbc97dbe1e0afc4d9e405275dbba80e
Liam Adamson <liamadamson1996@gmail.com>
2022-12-28 17:02:13 +0000

    Make mypy and pylint scan the tests folder too

--- scripts/testing/run_mypy.sh
+++ scripts/testing/run_mypy.sh
@@ -1,3 +1,3 @@
 # To be run from the main folder.
 
-docker run --rm -it -v "$PWD":"/code/" oop-patterns-in-python mypy --strict ./src/
+docker run --rm -it -v "$PWD":"/code/" oop-patterns-in-python mypy --strict ./src/ ./tests/
--- scripts/testing/run_pylint.sh
+++ scripts/testing/run_pylint.sh
@@ -1,3 +1,3 @@
 # To be run from the main folder.
 
-docker run --rm -it -v "$PWD/src":"/code/src" -v "$PWD/.pylintrc":"/code/.pylintrc" oop-patterns-in-python pylint ./src/
+docker run --rm -it -v "$PWD":"/code/" oop-patterns-in-python pylint ./src/ ./tests/
